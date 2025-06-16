088b2ace6b71805144f412c43fde7c31dd1a2008
9000Noemi <noemi.martinc@gmail.com>
2025-06-16 19:03:49 +0200

    WIP tasks service

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -11,5 +11,9 @@ export class TasksController {
         return this.tasksService.getAllTasks()
     }
 
+    createTask(){
+        //this.tasksService.createTask()
+    }
+
 
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,9 +1,17 @@
+//Servicio encargado de manejar la lógica relacionada con las tareas
+
+
 import { Injectable } from '@nestjs/common';
 import { Task, TaskStatus } from './task.entity';
+import { v4 } from 'uuid'
 
+
+//Decorador injectable: marca esta clase como inyectable,lo que permite usarla en controladores u otros servicios mediante inyección de dependencias.
 @Injectable()
+//CLASE que contiene toda la lógica para gestionar las tareas (crear, obtener, modificar, eliminar).
 export class TasksService {
 
+  //Array privado de tareas. Se inicializa con una tarea de ejemplo.
   private tasks: Task[] = [
     {
       id: '1',
@@ -13,10 +21,23 @@ export class TasksService {
     },
   ];
 
+  //Método que devuelve todas las tareas. Equivalente a un GET /tasks
   getAllTasks() {
+    //En una clase, this hace referencia a la instancia actual de esa clase.
     return this.tasks;
   }
-  createTask() {}
+
+  createTask(title: string, description: string) {
+    const task = {
+      id: v4(),
+      title,
+      description,
+      status: TaskStatus.PENDING
+    }
+    this.tasks.push(task);
+
+    return task;
+  }
   updateTask() {}
   deleteTask() {}
 }
